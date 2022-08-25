5a121aba2b57cdbf1b46d5fab7d28d55e96ed184
archis <vazearchis@gmail.com>
2022-08-25 09:18:44 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Notification Message
+![](https://i.imgur.com/jIh4d0F.png)
+
+When clicked on a button, display a notification message for 2 secs, and then hide it.
+If user hovers on the notification then it should stay for as long as the pointer is on it, once the pointer leaves then it hides after 2s.
+
---
+++ main.js
@@ -0,0 +1,83 @@
+//buttons
+let errorButton = document.querySelector(".error-btn");
+let warningButton = document.querySelector(".warning-btn");
+let infoButton = document.querySelector(".info-btn");
+let successButton = document.querySelector(".success-btn");
+//alerts
+let errorAlert = document.querySelector(".error")
+let warningAlert = document.querySelector(".warning")
+let infoAlert = document.querySelector(".info")
+let successAlert = document.querySelector(".success")
+
+
+let errorFlag = false;
+let warningFlag = false;
+let infoFlag = false;
+let succesFlag = false;
+
+errorButton.addEventListener("click", (e) => {
+    errorAlert.classList.add("show")
+    setTimeout(() => {
+        if (errorFlag !== true) {
+            errorAlert.classList.remove("show")
+        }
+    }, 2000)
+})
+warningButton.addEventListener("click", (e) => {
+    warningAlert.classList.add("show")
+    setTimeout(() => {
+        if (warningFlag !== true) {
+            warningAlert.classList.remove("show")
+        }
+    }, 2000)
+})
+infoButton.addEventListener("click", (e) => {
+    infoAlert.classList.add("show")
+    setTimeout(() => {
+        if (infoFlag !== true) {
+            infoAlert.classList.remove("show")
+        }
+    }, 2000)
+})
+successButton.addEventListener("click", (e) => {
+    successAlert.classList.add("show")
+    setTimeout(() => {
+        if (succesFlag !== true) {
+            successAlert.classList.remove("show")
+        }
+    }, 2000)
+})
+
+
+//errorAlert mouse over and leave handler
+errorAlert.addEventListener('mouseover', (e) => {
+    errorFlag = true;
+})
+errorAlert.addEventListener('mouseleave', (e) => {
+    errorFlag = false;
+    errorAlert.classList.remove("show")
+})
+//warningAlert mouse over and leave handler
+warningAlert.addEventListener('mouseover', (e) => {
+    warningFlag = true;
+})
+warningAlert.addEventListener('mouseleave', (e) => {
+    warningFlag = false;
+    warningAlert.classList.remove("show")
+})
+//infoAlert mouse over and leave handler
+infoAlert.addEventListener('mouseover', (e) => {
+    infoFlag = true;
+})
+infoAlert.addEventListener('mouseleave', (e) => {
+    infoFlag = false;
+    infoAlert.classList.remove("show")
+})
+//successAlert mouse over and leave handler
+successAlert.addEventListener('mouseover', (e) => {
+    succesFlag = true;
+})
+successAlert.addEventListener('mouseleave', (e) => {
+    succesFlag = false;
+    successAlert.classList.remove("show")
+})
